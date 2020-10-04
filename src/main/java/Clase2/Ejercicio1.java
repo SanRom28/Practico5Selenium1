dd274b4f2b36e8fd86a57cfe9102c6e86c51092a
Sandra <sandra_romeo10@hotmail.com>
2020-10-04 16:34:40 -0300

    se suben nuevos cambios

---
+++ src/main/java/Clase2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Clase2;
+
+public class Ejercicio1 {
+    //Mostrar Hola Mundo en pantalla
+
+
+    //INICIO
+    public static void main(String arg[]) {
+//MOSTRAR “Holamundo”;
+        System.out.println("Hola Mundo");
+        //FIN
+    }
+}
